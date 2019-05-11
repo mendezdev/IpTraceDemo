36983d6780e13039fcfd1dbb9d424bce9343ba3f
Pablo Mendez <pablo.mendez@emerios.com>
2019-05-11 14:07:06 -0300

    added ip route to the pipeline and body parser

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const ipRouter = require('./routers/ip');
+
+app.use('/api/ip', ipRouter);
 app.use('/', express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 3000;
