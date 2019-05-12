e3abca801e4d58f6df748b5117fb0b8bbc220a39
Pablo Mendez <pablo.mendez@emerios.com>
2019-05-12 18:27:23 -0300

    updated logic to response and added comments to metrics

--- db/country-metric.js
+++ db/country-metric.js
@@ -17,20 +17,37 @@ exports.getAll = () => {
 }
 
 exports.getMLReport = async () => {
+    // get the metrics order by distance desc
+    // so the first will be the further and the last one
+    // will be the closest
     const metricsDesc = await CountryMetric
         .find()
         .sort({ distance: 'desc' })
         .exec();
+    
+    // define the default value
+    const defaultValue = '--';
+    const response = {
+        further: defaultValue,
+        closest: defaultValue,
+        average: defaultValue
+    };
 
+    // if there is no metrics yet, then return the default
+    if (metricsDesc.length < 0) return response;
+
+    // by order, the first is the further
     const further = metricsDesc[0];
     let closest = null;
 
     const closestIndex = metricsDesc.length - 1;
 
+    // logic to know if there is only one metric at the moment
     closest = (closestIndex < 0 || closestIndex === 0) ?
         closest = metricsDesc[0] :
         closest = metricsDesc[closestIndex];
 
+    // logic to get the average
     const distanceSum = metricsDesc.reduce((prev, current) => {
         prev = prev + current.distance;
 
@@ -40,8 +57,8 @@ exports.getMLReport = async () => {
     const average = utils.round(distanceSum / metricsDesc.length, 2);
 
     return {
-        further,
-        closest,
-        average
+        further: utils.round(further.distance, 2).toString(),
+        closest: utils.round(closest.distance, 2).toString(),
+        average: utils.round(average, 2).toString()
     }
 }
